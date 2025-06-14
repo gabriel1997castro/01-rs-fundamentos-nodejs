56829c8fe6adc8ffe29ff189cf2e34ae39f57980
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-06-14 20:35:16 -0300

    creating unique and universal ID (UUID)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { Database } from './database.js';
+import { randomUUID } from 'node:crypto';
 
 const database = new Database();
 
@@ -17,7 +18,7 @@ const server = http.createServer(async (req, res) => {
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
 
-    const user = { id: 1, name, email }
+    const user = { id: randomUUID(), name, email }
     database.insert('users', user)
     return res.writeHead(201).end();
   }
