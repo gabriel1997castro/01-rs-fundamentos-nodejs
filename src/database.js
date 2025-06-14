5dd294e25c74daf1a7207a6eee83a487e483a9ca
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-06-14 20:31:43 -0300

    persisting data

--- src/database.js
+++ src/database.js
@@ -1,19 +1,42 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('db.json', import.meta.url)
+
 export class Database {
   // # make it private
   #database = {}
 
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        // If the file does not exist, initialize an empty database
+        this.#persist()
+
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+      .catch(err => console.error('Error writing to database:', err))
+  }
+
 
   select(table) {
-    const data = this.database[table] ?? []
+    const data = this.#database[table] ?? []
     return data
   }
 
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
+    if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data)
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
     return data
   }
 }
