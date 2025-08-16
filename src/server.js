f71bf06f1f4ababa55d32aa210aa390014bacbe0
Gabriel Castro <gabriel1997.castro@gmail.com>
2025-08-16 14:36:46 -0300

    Adds user deletion functionality

--- src/server.js
+++ src/server.js
@@ -13,8 +13,9 @@ const server = http.createServer(async (req, res) => {
 
   if (route) {
     const routeParams = req.url.match(route.path)
+    const params = { ...routeParams.groups }
 
-    console.log(routeParams)
+    req.params = params
 
     return route.handler(req, res);
   }
